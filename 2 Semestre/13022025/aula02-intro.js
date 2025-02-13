c023fec021e2ff82023da3eb7f4ea12f10f6fef4
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-13 08:32:50 -0300

    reorganizando

---
+++ 2 Semestre/13022025/aula02-intro.js
@@ -0,0 +1,31 @@
+//Concat - concatenar
+const alunos = ["Felipe", "Ana", "Pedro", "Beatriz"]
+const professores = ["Fernando", "Douglas", "Leo"]
+
+alunos.splice(2)
+console.log(alunos)
+
+//Método para adicionar um item no array: PUSH
+//professores.push("João")
+//console.log(professores)
+
+//Alterando a posição 1 do vetor, para Francisco
+//professores[1] = "Francisco"
+//console.log(professores)
+
+//Ordenando o vetor
+//alunos.sort()
+//console.log(alunos)
+
+//Deletando um item no array
+//delete alunos[0]
+//console.log(alunos)
+
+//Removendo o último item do array - POP()
+//alunos.pop()
+//console.log(alunos)
+
+//Removendo o primeiro item do array - SHIFT()
+//alunos.shift()
+//console.log(alunos)
+
