8ceea9aa81ada39e40f7f8c9e3770eb1536fc3e1
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-13 09:45:05 -0300

    aula 13022025

--- 2 Semestre/13022025/aula02-intro.js
+++ 2 Semestre/13022025/aula02-intro.js
@@ -1,9 +1,36 @@
 //Concat - concatenar
-const alunos = ["Felipe", "Ana", "Pedro", "Beatriz"]
-const professores = ["Fernando", "Douglas", "Leo"]
+// const alunos = [
+//     {nomeAluno: "Felipe", nota: 5, participativa: false},
+//     {nomeAluno: "Ana", nota: 9, participativa: true},
+//     {nomeAluno: "Pedro", nota: 3, participativa: true},
+//     {nomeAluno: "Beatriz", nota: 7, participativa: true},
+//     {nomeAluno: "Felipe", nota: 5, participativa: false},
+// ]
 
-alunos.splice(2)
-console.log(alunos)
+// const part= p => p.participativo
+// const nota = n => n.nota<5 
+
+// console.log(alunos.filter(part).filter(nota))
+
+//Filtro que retorna o Aluno que tem nota > 5
+// console.log(alunos.filter(function(e){
+//     return e.nota>5
+// }))
+
+//Filtro que retorna o Aluno que é participativo e nota < 5
+// console.log(alunos.filter(function(e){
+//     return e.participativo==false&&e.nota<5
+// }))
+
+
+// const professores = ["Fernando", "Douglas", "Leo"]
+
+// alunos.forEach(function(nome, indice){
+//     console.log(nome, indice)
+// })
+
+// alunos.splice(6,0,"Luis")
+// console.log(alunos)
 
 //Método para adicionar um item no array: PUSH
 //professores.push("João")
@@ -29,3 +56,26 @@ console.log(alunos)
 //alunos.shift()
 //console.log(alunos)
 
+
+
+// DESAFIOS
+
+// Arrow function que receba dois números e retorno o produto
+const multiplicar = (num1, num2) => num1 * num2;
+console.log(multiplicar(10,10));
+
+//Função que recebe dois números e retorne  soma, subtração, multiplicação e divisão dos números em um objeto
+// function operacoesMat(x,y){
+//     return {
+//         soma: x + y,
+//         subtracao: x - y,
+//         multiplicacao: x * y,
+//         divisao: x / y
+//     }
+// }
+// console.log(operacoesMat(5,5))
+
+function verificarNum(num){
+    return num > 0 ? "Positivo" : num < 0 ? "Negativo":"Zero"
+}
+console.log(verificarNum(-33))
